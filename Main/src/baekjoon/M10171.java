03f9d53e4b22950780bf95adfabfbef79e0a038d
Batwan01 <wldhks1120@naver.com>
2023-04-16 20:19:23 +0900

    M10171

--- Main/src/baekjoon/M10171.java
+++ Main/src/baekjoon/M10171.java
@@ -2,9 +2,10 @@ package baekjoon;
 
 public class M10171 {
 	public static void main(String[] args) {
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  )");
-		System.out.println(" \\(__)|");
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
 	}
 }
