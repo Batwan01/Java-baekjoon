de26520e945ff35a46012e8d48e7c1f9d143ffd7
박지완 <@192.168.219.101>
2023-04-29 01:58:01 +0900

    M10950

---
+++ Java/src/main/M10950.java
@@ -0,0 +1,19 @@
+package main;
+import java.io.*;
+import java.util.*;
+public class M10950 {
+	public static void main(String[] args)throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int a,b;
+		for(int i=0; i<n ; i++) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			arr[i] = a+b;
+		}
+		for(int i=0; i<n ; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
