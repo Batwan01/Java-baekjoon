3432369426756a99ef6dbb2182c78581936fa3a4
Batwan01 <wldhks1120@naver.com>
2023-05-13 22:40:53 +0900

    M2743

---
+++ Main/src/baekjoon/M2743.java
@@ -0,0 +1,12 @@
+package baekjoon;
+import java.util.*;
+
+public class M2743 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.nextLine();
+		System.out.print(word.length());
+		
+		sc.close();
+	}
+}
