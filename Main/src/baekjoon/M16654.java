b09f7798dac8a637cc160ce1fc3a256401fc05c5
Batwan01 <wldhks1120@naver.com>
2023-05-14 00:23:29 +0900

    M16654

---
+++ Main/src/baekjoon/M16654.java
@@ -0,0 +1,9 @@
+package baekjoon;
+import java.io.*;
+
+public class M16654 {
+	public static void main(String[] args)throws IOException {
+		int a = System.in.read();
+		System.out.println(a);
+	}
+}
