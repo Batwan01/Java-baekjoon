b41a811b884fe86e95f5b235604d05d4c2f918ef
Batwan01 <wldhks1120@naver.com>
2023-05-15 18:50:52 +0900

    M1157

---
+++ Main/src/baekjoon/M1157.java
@@ -0,0 +1,32 @@
+package baekjoon;
+import java.util.*;
+
+public class M1157 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		char words;
+		int num, max=0, pos=0;
+		int[] alpha = new int[26];
+		for(int i = 0; i<word.length(); i++) {
+			if(word.charAt(i) >= 97) {
+				num = word.charAt(i) - 97;
+				alpha[num]++;
+			}
+			else {
+				num = word.charAt(i) - 65;
+				alpha[num]++;
+			}
+		}
+		for(int i = 0; i<26; i++) {
+			max = max>alpha[i] ? max:alpha[i];
+			if(max == alpha[i]) pos = i;
+		}
+		int count = 0;
+		for(int i = 0; i<26; i++) {
+			if(max == alpha[i]) count++;
+		}
+		if(count == 1) System.out.printf("%c",pos+65);
+		else System.out.println("?");
+	}
+}
