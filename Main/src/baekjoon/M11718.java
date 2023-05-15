0196ff7435da10e197434e07a67f3ac250efb080
Batwan01 <wldhks1120@naver.com>
2023-05-15 16:07:08 +0900

    M11718

---
+++ Main/src/baekjoon/M11718.java
@@ -0,0 +1,14 @@
+package baekjoon;
+import java.util.*;
+
+public class M11718 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word;
+		while(sc.hasNextLine()) {
+			word = sc.nextLine();
+			System.out.println(word);
+		}
+		sc.close();
+	} 
+}
