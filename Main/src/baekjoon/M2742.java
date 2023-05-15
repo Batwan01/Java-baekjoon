cde59d1336125aaa579a2809ea5b97740e75f2cf
Batwan01 <wldhks1120@naver.com>
2023-05-15 19:19:23 +0900

    M2742

---
+++ Main/src/baekjoon/M2742.java
@@ -0,0 +1,13 @@
+package baekjoon;
+import java.util.*;
+
+public class M2742 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = N; i>0; i--) {
+			System.out.println(i);
+		}
+		sc.close();
+	}
+}
