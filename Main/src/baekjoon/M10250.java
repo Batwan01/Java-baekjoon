c42293bddfa7bf6a6fef8260481e20594fe21d67
Batwan01 <wldhks1120@naver.com>
2023-09-07 17:06:56 +0900

    M10250

---
+++ Main/src/baekjoon/M10250.java
@@ -0,0 +1,11 @@
+package baekjoon;
+import java.util.*;
+
+public class M10250 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.print(a);
+		sc.close();
+	}
+}
