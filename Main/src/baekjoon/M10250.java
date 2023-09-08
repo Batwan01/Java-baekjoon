c919f4624c2b33aa4476c3804beb05d09f1a0a98
Batwan01 <wldhks1120@naver.com>
2023-09-08 11:35:27 +0900

    M10250

--- Main/src/baekjoon/M10250.java
+++ Main/src/baekjoon/M10250.java
@@ -4,8 +4,30 @@ import java.util.*;
 public class M10250 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		System.out.print(a);
+		int T = sc.nextInt();
+		
+		int floor, number;
+		for(int i=0; i<T; i++)
+		{
+			floor = 0;
+			int H = sc.nextInt();
+			int W = sc.nextInt();
+			int N = sc.nextInt();
+			
+			if(N%H == 0) 
+			{
+				floor = N*100; //층수 구하기
+				number = N/H +1; //호수 구하기
+			}
+			else 
+			{
+				floor = N%H*100; //층수 구하기
+				number = N/H +1; //호수 구하기
+			}
+			
+			System.out.print(floor + number);
+		}
+		
 		sc.close();
 	}
 }
