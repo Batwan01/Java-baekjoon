83f6baec20a22f68772e8160dfa1df063db64155
Batwan01 <wldhks1120@naver.com>
2023-09-23 17:20:27 +0900

    M1181

--- Main/src/baekjoon/M1181.java
+++ Main/src/baekjoon/M1181.java
@@ -4,14 +4,19 @@ import java.util.*;
 public class M1181 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		HashSet<String> set = new HashSet<String>();
 		int N = sc.nextInt();
 		String[] word = new String[N];
 		
 		for(int i = 0; i < N ; i++) {
-			word[i] = sc.nextLine();
+			word[i] = sc.next();
 		}
-		for(int i = 0; i < N ; i++) {
-			System.out.println(word[i]);
+		String[] words = Arrays.stream(word).distinct().toArray(String[]::new);
+		
+		Arrays.sort(words, Comparator.comparingInt(String::length)); //글자수 정렬
+		
+		for(int i = 0; i < words.length ; i++) {
+			System.out.println(words[i]);
 		}
 		
 		
