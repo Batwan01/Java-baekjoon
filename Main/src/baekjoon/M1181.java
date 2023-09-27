45355d3aa8054223e3b31fa8b2a575b103f1275d
Batwan01 <wldhks1120@naver.com>
2023-09-27 14:40:21 +0900

    M1181

--- Main/src/baekjoon/M1181.java
+++ Main/src/baekjoon/M1181.java
@@ -11,16 +11,24 @@ public class M1181 {
 		for(int i = 0; i < N ; i++) {
 			word[i] = sc.next();
 		}
-		String[] words = Arrays.stream(word).distinct().toArray(String[]::new); //배열 > stream > 중복제거 > 배열
 		
-		Arrays.sort(words, Comparator.comparingInt(String::length)); //글자수 정렬
-		
-		for(int i = 0; i < words.length ; i++) {
-			System.out.println(words[i]);
+		Arrays.sort(word, new Comparator<String>() {
+			
+			public int compare(String s1, String s2) {
+					if(s1.length() == s2.length()) {
+						return s1.compareTo(s2);
+					}
+					else {
+						return s1.length() - s2.length();
+					}
+			}
+		});
+		System.out.println(word[0]);
+		for(int i=1; i < N; i++) {
+			if(!word[i].equals(word[i-1])) System.out.println(word[i]);
 		}
-		
-		
 		
 		sc.close();
 	}
 }
+
