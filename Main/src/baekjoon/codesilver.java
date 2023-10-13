739dc782150dcdcd27ddbbf5c7d530b33866abc2
Batwan01 <wldhks1120@naver.com>
2023-10-13 22:28:39 +0900

    .

--- Main/src/baekjoon/codesilver.java
+++ Main/src/baekjoon/codesilver.java
@@ -1,5 +1,29 @@
 package baekjoon;
+import java.util.*;
+
+class Book_Mangement {
+	System.out.println("")
+	
+}
+
+class Book {
+	private String title;
+	private String writer;
+	private int price;
+	
+}
+
+class Ebook {
+	
+
+}
+
+class Magazine {
+	
+}
 
 public class codesilver {
-
+	public static void main(String[] args) {
+		Book_Mangement bm = new Book_Mangement();
+	}
 }
