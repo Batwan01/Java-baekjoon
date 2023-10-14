c6e9dd351aa5c5ca1f96fa7d40e443eb3136f09c
Batwan01 <wldhks1120@naver.com>
2023-10-14 15:58:25 +0900

    도서관리

--- Main/src/baekjoon/codesilver.java
+++ Main/src/baekjoon/codesilver.java
@@ -1,25 +1,41 @@
 package baekjoon;
 import java.util.*;
 
-class Book_Mangement {
-	System.out.println("")
+class Book_Mangement extends Book {
+	public Book_Mangement() {
+		System.out.println("==========도서관리 프로그램==========");
+		input();
+		find();
+	}
+	
+	public void input(){ //책 입력
+		Book book = new Book();
+		Ebook ebook = new Ebook();
+		Magazine magazine = new Magazine();
+	}
+	
+	public void find() { //책 찾기
+		
+	}
 	
 }
 
-class Book {
+class Book { 
 	private String title;
 	private String writer;
 	private int price;
+	public Book(String title, String writer, int price) {
+		
+	}
 	
 }
 
-class Ebook {
-	
-
+class Ebook extends Book{
+	private int key;
 }
 
-class Magazine {
-	
+class Magazine extends Book{
+	private int date;
 }
 
 public class codesilver {
